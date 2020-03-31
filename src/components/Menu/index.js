2b894c9446d0f3ee4ef2fba25c63ea87cf565c8f
Andre Garcia <andre.luiz@gec.inatel.br>
2020-03-30 22:54:30 -0300

    Uploading to github

---
+++ src/components/Menu/index.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+import QRCode from 'react-native-qrcode-svg'
+
+import { Container, Code, Nav, NavItem, NavText, SignOutButton, SignOutButtonText } from './styles'
+
+export default function Menu({ translateY }) {
+  return (
+    <Container style={{
+      opacity: translateY.interpolate({
+        inputRange: [0, 150],
+        outputRange: [0, 1]
+      })
+    }}>
+      <Code>
+        <QRCode
+          value="www.google.com"
+          size={80}
+          backgroundColor="#FFF"
+          color="#8B10AE"
+        />
+      </Code>
+
+      <Nav>
+        <NavItem>
+          <Icon name="help-outline" size={20} color="#FFF" />
+          <NavText>Me Ajuda</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="person-outline" size={20} color="#FFF" />
+          <NavText>Perfil</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="credit-card" size={20} color="#FFF" />
+          <NavText>Configurar cartão</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="smartphone" size={20} color="#FFF" />
+          <NavText>Configurações do app</NavText>
+        </NavItem>
+      </Nav>
+
+      <SignOutButton onPress={ () => {} }>
+        <SignOutButtonText>SAIR DO APP</SignOutButtonText>
+      </SignOutButton>
+
+    </Container>
+  )
+}
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,59 +1,101 @@
-import React from 'react';
+import React from 'react'
 
-import {
-  Text, Image, StyleSheet, Dimensions, ImageBackground, StatusBar,
-} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flex: 1,
-    paddingHorizontal: 20,
-  },
-  fileName: {
-    fontWeight: 'bold',
-    marginTop: 5,
-  },
-  instructions: {
-    color: '#DDD',
-    fontSize: 14,
-    marginTop: 20,
-    textAlign: 'center',
-  },
-  logo: {
-    height: Dimensions.get('window').height * 0.11,
-    marginVertical: Dimensions.get('window').height * 0.11,
-    width: Dimensions.get('window').height * 0.11 * (1950 / 662),
-  },
-  welcome: {
-    color: '#fff',
-    fontSize: 22,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
+import { Animated } from 'react-native'
+import { PanGestureHandler, State } from 'react-native-gesture-handler'
 
-const Main = () => (
-  <ImageBackground
-    source={{
-      uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/background.png',
-    }}
-    style={styles.container}
-    resizeMode="cover"
-  >
-    <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-    <Image
-      source={{
-        uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/rocketseat_logo.png',
-      }}
-      style={styles.logo}
-      resizeMode="contain"
-    />
-    <Text style={styles.welcome}>Bem-vindo ao Template Básico!</Text>
-    <Text style={styles.instructions}>Essa é a tela principal da sua aplicação =)</Text>
-    <Text style={styles.instructions}>Você pode editar a tela no arquivo:</Text>
-    <Text style={[styles.instructions, styles.fileName]}>src/pages/Main/index.js</Text>
-  </ImageBackground>
-);
+import Header from '~/components/Header'
+import Tabs from '~/components/Tabs'
+import Menu from '~/components/Menu'
 
-export default Main;
+import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles'
+
+
+export default function Main() {
+  let offset = 0
+
+  const translateY = new Animated.Value(0)
+
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        }
+      }
+    ],
+    { useNativeDriver: true }
+  )
+
+  function onHandlerStateChange(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE){
+      let opened = false
+
+      const { translationY } = event.nativeEvent
+
+      offset += translationY
+
+      if (translationY === 100 || translationY > 100) {
+        opened = true
+      } else {
+        translateY.setValue(offset)
+        translateY.setOffset(0)
+        offset = 0
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true
+      }).start(() => {
+        offset = opened ? 380 : 0
+        translateY.setOffset(offset)
+        translateY.setValue(0)
+      })
+    }
+  }
+
+  return (
+    <Container>
+      <Header />
+
+      <Content>
+        <Menu translateY={ translateY } />
+        
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChange}
+        >
+          <Card style={{
+            transform: [{
+              translateY: translateY.interpolate({
+                inputRange:[-350, 0, 380],
+                outputRange: [-50, 0, 380],
+                extrapolate: 'clamp'
+              })
+            }]
+          }}>
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ 1.000,00</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 20,00 recebida de André hoje às 10:00h.
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
+
+      </Content>
+
+      <Tabs translateY={ translateY } />
+    </Container>
+  )
+}
+
